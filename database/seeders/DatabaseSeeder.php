7f3fabad725ef805524a41a3e3e066839bc68e51
IamHuin <tatienhung2003@gmail.com>
2026-07-08 17:23:33 +0700

    a

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,19 +12,16 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'jira_username' => 'admin',
-            'jira_password' => Hash::make('admin'),
-            'jira_display_name' => 'Super Admin',
-            'super_admin' => IsAdminEnum::YES,
-            'is_admin' => IsAdminEnum::NO,
-        ]);
+        User::updateOrCreate(
+            ['jira_username' => 'admin'],
+            [
+                'jira_password'      => Hash::make('admin'),
+                'jira_display_name'  => 'Super Admin',
+                'super_admin'        => IsAdminEnum::YES,
+                'is_admin'           => IsAdminEnum::NO,
+            ]
+        );
     }
 }
